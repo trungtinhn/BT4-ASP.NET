01b3ced80e261e148dd052f0e572459a73145e9a
trungtinhn <trungtinh1620@gmail.com>
2024-10-22 11:30:38 +0700

    Add project files.

---
+++ BT4_ASP.NET/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using BT4_ASP.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT4_ASP.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            string connectionString = "Data Source=DESKTOP-SD252UH;Initial Catalog=QuanLySanPham;Integrated Security=True";
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connectionString);
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ BT4_ASP.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BT4_ASP.NET.MvcApplication" Language="C#" %>
